d09422f161de22a797edf07487664571dbd08d3b
Rigardt Engelbrecht <rigardtengelbrecht@aComputerHasNoName.local>
2019-05-22 10:27:32 +0200

    done, figured out more efficient way. if sure, del commented code

---
+++ ft_strcmp.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char) *s2);
+	/*int i;
+	int res;
+
+	i = 0;
+	res = 0;
+	while (s1[i] != '\0')
+	{
+
+		res += s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		res -= s2[i];
+		i++;
+	}
+	return (res);
+	*/
+}
+
+int main()
+{
+	char str1[] = "word";
+	char str2[] = "wurd";
+
+	printf("%d\n", ft_strcmp(str1, str2));
+
+	return 0;
+}
