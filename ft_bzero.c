caa2c87ad28090395d8e2129e80051da75579a5e
Rigardt Engelbrecht <rigardtengelbrecht@aComputerHasNoName.local>
2019-05-25 13:40:18 +0200

    simplified code

--- ft_bzero.c
+++ ft_bzero.c
@@ -2,21 +2,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char *temp;
-	size_t i;
-
-	i = 0;
-	temp = NULL;
-	*temp = (unsigned char)s;
-	if (n > 0)
-	{
-		while (i < n)
-		{
-//			temp[i] = 0;
-//			temp++;
-//			i++;
-			ft_memset(temp, 0, n);		
-		}
-		
-	}
+	ft_memset(s, 0, n);
 }
