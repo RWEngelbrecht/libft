fa75457405bf8f0d49c770ff368828c55d748fe1
Rigardt Engelbrecht <rigardtengelbrecht@aComputerHasNoName.local>
2019-05-25 14:14:26 +0200

    removed main

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,7 +12,7 @@ int	ft_atoi(const char *str)
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	if (str[i] == '-')
-		neg = 1; 
+		neg = 1;
 	if (str[i] == '+' || str[i] == '-')
 		i++;
 	while (*str && (str[i] >= '0' && str[i] <= '9'))
@@ -24,12 +24,4 @@ int	ft_atoi(const char *str)
 		return (-n);
 	else
 		return (n);
-}
-
-int main()
-{
-	char *number = "123";
-
-	printf("\'%s\' is a string.\nWith the following words, the string will be magically transmutated into integer values based on the representative character:\nBy the Spirit of the Sun, the emisaries of the Void Realms!\nBy the Fires of Attrition and the Nosehairs of the Sanguine Sages,\nI command thee: ATOI!\n*poof*\n%d\n...\nSee? It is an integer now.\n", number, atoi(number));
-	return (0);
 }
