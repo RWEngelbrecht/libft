9fcd07f4a08c764f8866e972e974061a4018a688
Rigardt Engelbrecht <rigardtengelbrecht@aComputerHasNoName.local>
2019-05-25 13:13:13 +0200

    removed main for unit testing

--- ft_strchr.c
+++ ft_strchr.c
@@ -15,12 +15,3 @@ char	*ft_strchr(const char *s, int c)
 		return ((char*)(s + i));
 	return (NULL);
 }
-
-int main()
-{
-	char str[] = "word";
-
-	printf("%s\n", ft_strchr(str, ''));
-
-	return 0;
-}
