6c32977565bb95932c59f8c13b2cc5cb22cadb76
Rigardt Engelbrecht <rengelbr@c3r8s9.wethinkcode.co.za>
2019-05-27 09:49:25 +0200

    done for now, check if ft_strcat can be used

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:13:35 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/27 09:14:25 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/27 09:45:51 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@
 char * ft_strjoin(char const *s1, char const *s2)
 {
     char *str;
-    //int i;
-    //int j;
+    int i;
+    int j;
 
-    str = ft_strcat(s1, s2);
-    /*
-    str = ft_memalloc((ft_strlen(s1) + ft_strlen(s2));
+    //str = ft_strcat(s1, s2);
+    str = ft_memalloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
     i = 0;
     j = 0;
     if (!str)
@@ -37,7 +36,6 @@ char * ft_strjoin(char const *s1, char const *s2)
         j++;
     }
     str[i] = '\0';
-    */
     if (!str)
         return (NULL);
     else
