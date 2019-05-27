0af8244b1eeaa7bd12a63acf6d24802a01ff055e
Rigardt Engelbrecht <rigardtengelbrecht@aComputerHasNoName.local>
2019-05-27 19:17:41 +0200

    use ft_strnew to allocate mem. still not compatible with neg ints

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,45 +6,30 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:17:59 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/27 17:37:04 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/27 19:14:56 by rigardtengelbrecht###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+int		countchars(int n)
 {
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	if (n == 0)
 		i++;
+	else if (n < 0)
+		i++;
+	else
+	{
+		while (n > 0)
+		{
+			i++;
+			n /= 10;
+		}
+	}
 	return (i);
-}
-
-char *ft_strrev(char *s)
-{
-    size_t n;
-    size_t size;
-    size_t half;
-    size_t size2;
-    char str;
-
-    n = 0;
-    size = ft_strlen(s);
-    half = size / 2;
-    if (s)
-    {
-        while (n < half)
-        {
-            size2 = size - n - 1;
-            str = s[n];
-            s[n] = s[size2];
-            s[size2] = str;
-            n++;
-        }
-    }
-    return (s);
 }
 
 char    *ft_itoa(int n)
@@ -53,7 +38,7 @@ char    *ft_itoa(int n)
     int     i;
     int     neg;
 
-    str = ft_strnew();
+    str = ft_strnew(countchars(n));
     i = 0;
     neg = 0;
     if (n == 0)
