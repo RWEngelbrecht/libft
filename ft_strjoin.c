4d55b2454b3275aaa35119ce3f40eb0c8f3b9980
Rigardt Engelbrecht <rengelbr@c3r8s9.wethinkcode.co.za>
2019-05-28 16:55:45 +0200

    not done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,11 +6,13 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:13:35 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/27 09:45:51 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/28 14:03:31 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+
 
 char * ft_strjoin(char const *s1, char const *s2)
 {
@@ -19,12 +21,14 @@ char * ft_strjoin(char const *s1, char const *s2)
     int j;
 
     //str = ft_strcat(s1, s2);
-    str = ft_memalloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+    str = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
     i = 0;
     j = 0;
     if (!str)
         return (NULL);
-    while (s1[i] != '\0')
+    ft_strcpy(str, s1);
+    ft_strcat(str, s2);
+    /*while (s1[i] != '\0')
     {
         str[i] = s1[i];
         i++;
@@ -34,8 +38,8 @@ char * ft_strjoin(char const *s1, char const *s2)
         str[i] = s2[j];
         i++;
         j++;
-    }
-    str[i] = '\0';
+    }*/
+    //str[i] = '\0';
     if (!str)
         return (NULL);
     else
