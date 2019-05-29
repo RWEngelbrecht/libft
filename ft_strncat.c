b16b76a329232ca91c780c909d42f0b3c55e82bd
Rigardt Engelbrecht <rengelbr@c3r8s9.wethinkcode.co.za>
2019-05-29 14:11:27 +0200

    fixed

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 14:06:37 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/29 14:08:06 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/29 14:09:41 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,18 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	char *temp;
 	size_t i;
 	size_t j;
 
-	temp = (char *)malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2) + 1)));
 	i = 0;
 	j = 0;
-	while (s1[j] != '\0')
-	{
-		temp[i] = s1[j];
+	while (s1[i] != '\0')
 		i++;
-		j++;
-	}
-	j = 0;
 	while (s2[j] != '\0' && j < n)
 	{
-		temp[i] = s2[j];
-		i++;
+		s1[i + j] = s2[j];
 		j++;
 	}
-	temp[i] = '\0';
-	s1 = temp;
-
+	s1[i + j] = '\0';
 	return (s1);
 }
