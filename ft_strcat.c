ab5232993cd19bf19ca29d9cd083d3853923a3f0
Rigardt Engelbrecht <rengelbr@c3r8s9.wethinkcode.co.za>
2019-05-29 14:12:05 +0200

    removed commented code

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 14:24:24 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/29 14:04:52 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/29 14:11:53 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,13 @@
 
 char	*ft_strcat(char *s1, const char *s2)
 {
-	//char *temp;
 	int i;
 	int j;
 
-	//temp = (char *)malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2) + 1)));
 	i = 0;
 	j = 0;
 	while (s1[i] != '\0')
-	{
-		//temp[i] = s1[j];
 		i++;
-		//j++;
-	}
 	while (s2[j] != '\0')
 	{
 		s1[i + j] = s2[j];
