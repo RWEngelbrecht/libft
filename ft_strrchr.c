a8ce4ebd936295ebef51bf7f77448d2d9afb67e8
Rigardt Engelbrecht <rengelbr@c3r8s9.wethinkcode.co.za>
2019-05-29 16:12:31 +0200

    still not working fully

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,22 +6,41 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 14:12:46 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/29 14:13:02 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/29 15:59:01 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 char *ft_strrchr(const char *s, int c)
 {
-	int		i ;
+	size_t		i ;
 
-	i = (int)ft_strlen(s) + 1;
-	while (i > 0)
+	i = ft_strlen(s);
+	if (!i)
+		return (NULL);
+	while (i >= 0)
+	{
+		if (*(s + i) == (char)c)
+			return ((char *)(s+i));
+		i--;
+	}
+	/*
+	while (--i >= 1)
 	{
 		if (*(s + i) == (char)c)
 			return ((char *)(s + i));
-		i--;
-	}
+	}*/
 	return (NULL);
+
+}
+
+int main()
+{
+	char *src = "";
+	char *d1 = strrchr(src, 'a');
+	char *d2 = ft_strrchr(src, 'a');
+
+	printf("%s\n", d1);
+	printf("%s\n", d2);
 }
