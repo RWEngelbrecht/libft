559a045bc301bc1dcfbfbd34556e564550ccff56
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-05-30 13:25:40 +0200

    didn't work, does now

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,22 +6,35 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:08:20 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/30 10:08:21 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/30 13:25:07 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *stack, const char *needle, size_t len)
+char	*ft_strnstr(const char *stack, const char *needle, size_t len)
 {
 	size_t	nd_len;
-	int		last_res;
+	size_t	i;
+	//int		last_res;
 
-	last_res = 1;
+	//last_res = 1;
 	nd_len = ft_strlen(needle);
-	if (nd_len == 0)
+	i = 0;
+	if (nd_len == 0 || needle == stack)
 		return ((char *)stack);
-	while (nd_len <= len && *stack != '\0' && (last_res = ft_strncmp(stack, needle, nd_len)))
+	while (len > 0 && stack[i] != '\0')
+	{
+		//if(ft_strstr(stack, needle) && len != 0)
+		if (!(ft_strncmp((stack + i), needle, nd_len)) && (len > (len - i)))
+			return ((char*)ft_strstr(stack, needle));
+		len--;
+		i++;
+	}
+	return (NULL);
+	/*
+	while (nd_len <= len && *stack != '\0' &&
+			(last_res = ft_strncmp(stack, needle, nd_len)))
 	{
 		len--;
 		stack++;
@@ -30,4 +43,5 @@ char *ft_strnstr(const char *stack, const char *needle, size_t len)
 		return (NULL);
 	else
 		return ((char *)stack);
+	*/
 }
