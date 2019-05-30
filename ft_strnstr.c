b1bb6a6a252f77955b9f3035fc3374180782b17c
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-05-30 13:26:37 +0200

    removed commented code

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:08:20 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/30 13:25:07 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/30 13:26:19 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,17 @@ char	*ft_strnstr(const char *stack, const char *needle, size_t len)
 {
 	size_t	nd_len;
 	size_t	i;
-	//int		last_res;
 
-	//last_res = 1;
 	nd_len = ft_strlen(needle);
 	i = 0;
 	if (nd_len == 0 || needle == stack)
 		return ((char *)stack);
 	while (len > 0 && stack[i] != '\0')
 	{
-		//if(ft_strstr(stack, needle) && len != 0)
 		if (!(ft_strncmp((stack + i), needle, nd_len)) && (len > (len - i)))
 			return ((char*)ft_strstr(stack, needle));
 		len--;
 		i++;
 	}
 	return (NULL);
-	/*
-	while (nd_len <= len && *stack != '\0' &&
-			(last_res = ft_strncmp(stack, needle, nd_len)))
-	{
-		len--;
-		stack++;
-	}
-	if (last_res != 0)
-		return (NULL);
-	else
-		return ((char *)stack);
-	*/
 }
