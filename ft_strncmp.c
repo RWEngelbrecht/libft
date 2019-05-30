ef65c1f39bc25a781a70dbec03f954b6ee2dbf1c
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-05-30 10:59:45 +0200

    formatting

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:08:09 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/30 10:08:10 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/30 10:58:05 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n > 0 && *s1 != '\0' && *s1 == *s2)
 	{
