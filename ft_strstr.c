d2b7eb780647ff6dc43d239c273448e84f2c85a2
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-05-30 10:59:21 +0200

    didn't work. made it work

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,46 +6,27 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:08:36 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/30 10:08:37 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/30 10:57:58 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char *ft_strstr(const char *stack, const char *needle)
+char	*ft_strstr(const char *stack, const char *needle)
 {
 	int		i;
-	int		j;
-	int		k;
 	int		nd_len;
-	char	*str;
-	char	*ndl;
 
 	i = 0;
-	j = 0;
-	k = 0;
 	nd_len = ft_strlen(needle);
-	str = (char*)stack;
-	ndl = (char*)needle;
 	if (nd_len == 0)
-		return (str);
-	while (str[i] != '\0')
+		return ((char*)stack);
+	while (stack[i] != '\0')
 	{
-		if (str[i] == ndl[j])
-		{
-			k = i;
-			while (nd_len > 0)
-			{
-				if (str[k] != ndl[j])
-					return (NULL);
-				k++;
-				j++;
-				nd_len--;
-			}
-			return(&str[i]);
-		}
-		else
-			i++;
+		if (!(ft_strncmp((stack + i), needle, nd_len)))
+			return ((char*)(stack + i));
+		i++;
 	}
 	return (NULL);
 }
