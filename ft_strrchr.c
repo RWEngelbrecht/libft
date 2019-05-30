05bec1d5bd8f63c71d56f490ec6c6ca02570d8fd
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-05-30 11:30:43 +0200

    removed commented code

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 14:12:46 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/30 10:20:07 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/30 11:29:53 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 #include <stdio.h>
 char *ft_strrchr(const char *s, int c)
 {
-	//size_t		i ;
-
-	//i = ft_strlen(s);
 	if (!ft_strchr(s, c))
 		return (NULL);
 	while (*s)
@@ -24,30 +21,4 @@ char *ft_strrchr(const char *s, int c)
 	while (*s != c)
 		s--;
 	return ((char*)s);
-	/*if (!i)
-		return (NULL);
-	while (i >= 0)
-	{
-		if (*(s + i) == (char)c)
-			return ((char *)(s+i));
-		i--;
-	}
-
-	while (--i >= 1)
-	{
-		if (*(s + i) == (char)c)
-			return ((char *)(s + i));
-	}*/
-	//return (NULL);
-
-}
-
-int main()
-{
-	char *src = "abbbbbbbbbbbbbbb";
-	char *d1 = strrchr(src, 'a');
-	char *d2 = ft_strrchr(src, 'a');
-
-	printf("%s\n", d1);
-	printf("%s\n", d2);
 }
