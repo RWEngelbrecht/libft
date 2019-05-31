53315afb51076894c4acf25acb798c2f0cb4465d
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-05-31 14:25:03 +0200

    works for the most part. does it work with full whitespace input?

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,14 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:14 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/27 15:23:07 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/31 14:23:59 by rigardtengelbrecht###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char *  ft_strtrim(char const *s)
+char    *ft_strtrim(char const *s)
 {
     char *trimd;
     int i;
@@ -20,16 +21,26 @@ char *  ft_strtrim(char const *s)
     int k;
 
     i = 0;
-    j = ft_strlen(s);
+    j = ft_strlen(s) - 1;
     k = 0;
     if (!s)
         return (NULL);
-    while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-        i++;
-    while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
-        j--;
-    trimd = (char *)malloc(sizeof(char) * (j - i + 1));
-    while (i < j)
+    if (ft_iswhitespace(s[i]) || ft_iswhitespace(s[j]))
+    {
+        while (ft_iswhitespace(s[i]))
+            i++;
+        while (ft_iswhitespace(s[j]))
+            j--;
+    }
+    else
+    {
+        trimd = ft_strnew(j);
+        return (ft_strcpy(trimd, s));
+    }
+    trimd = ft_strnew((j - i));
+    if (!trimd)
+        return (0);
+    while (i <= j)
     {
             trimd[k] = s[i];
             i++;
