018ea7dfccfb669f4f5829fbd9d4f11e8cbc4429
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-05-31 11:03:51 +0200

    made safer + changed from pointer- to index incrementation

--- ft_striteri.c
+++ ft_striteri.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 15:15:53 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/23 15:15:58 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/05/31 11:01:04 by rigardtengelbrecht###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	unsigned int i;
 
 	i = 0;
-	while (*s != '\0')
+	if (s)
 	{
-		f(i, &s[i]);
-		s++;
-		i++;
+		while (s[i] != '\0')
+		{
+			f(i, &s[i]);
+			i++;
+		}
 	}
 }
