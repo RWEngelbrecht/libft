f7673f6425d81ed860d996709f7b3e79936e21f7
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-06-03 17:20:41 +0200

    fixed formatting for norminette

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 12:00:05 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/03 09:39:08 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/03 17:19:48 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,72 +14,72 @@
 
 static int     ft_wordcount(char const *str, char c)
 {
-    int     count;
-    int     i;
+	int     count;
+	int     i;
 
-    count = 0;
-    i = 0;
-    while (str[i] != '\0')
-    {
-        while (str[i] == c)
-            i++;
-        if (str[i] != c && str[i] != '\0')
-        {
-            count++;
-            i++;
-            while (str[i] != c && str[i] != '\0')
-                i++;
-        }
-    }
-    return (count);
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != c && str[i] != '\0')
+		{
+			count++;
+			i++;
+			while (str[i] != c && str[i] != '\0')
+				i++;
+		}
+	}
+	return (count);
 }
 
 static char     **ft_split(char const *s, char c, char **arr)
 {
-    int     l;
-    int     start;
-    int     i;
+	int     l;
+	int     start;
+	int     i;
 
-    l = 0;
-    start = 0;
-    i = 0;
-    while (s[l] != '\0')
-    {
-        if (s[l] != c)
-        {
-            start = l;
-            while (s[l] != c && s[l])
-                l++;
-            arr[i++] = ft_strsub(s, start, (l - start));
-        }
-        else
-            while (s[l] == c && s[l])
-                l++;
-        arr[i] = NULL;
-    }
-    return (arr);
+	l = 0;
+	start = 0;
+	i = 0;
+	while (s[l] != '\0')
+	{
+		if (s[l] != c)
+		{
+			start = l;
+			while (s[l] != c && s[l])
+				l++;
+			arr[i++] = ft_strsub(s, start, (l - start));
+		}
+		else
+			while (s[l] == c && s[l])
+				l++;
+		arr[i] = NULL;
+	}
+	return (arr);
 }
 
 char    **ft_strsplit(char const *s, char c)
 {
-    char    **arr;
+	char    **arr;
 
-    if (!s)
-    {
-        if (c != ' ')
-        {
-            if (!(arr = (char **)malloc(sizeof(char *))))
-                return (NULL);
-            arr[0] = NULL;
-            return (arr);
-        }
-    }
-    else if (!c)
-    {
-        return ((char **)ft_strdup(s));
-    }
-    if (!(arr = (char **)malloc(sizeof(*arr) * ft_wordcount(s, c) + 1)))
-        return (NULL);
-    arr = ft_split(s, c, arr);
-    return (arr);
+	if (!s)
+	{
+		if (c != ' ')
+		{
+			if (!(arr = (char **)malloc(sizeof(char *))))
+				return (NULL);
+			arr[0] = NULL;
+			return (arr);
+		}
+	}
+	else if (!c)
+	{
+		return ((char **)ft_strdup(s));
+	}
+	if (!(arr = (char **)malloc(sizeof(*arr) * ft_wordcount(s, c) + 1)))
+		return (NULL);
+	arr = ft_split(s, c, arr);
+	return (arr);
 }
