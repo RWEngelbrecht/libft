6ef577a3e97d2e8a9ab77f11e8e39d1d9ea1f9c2
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-06-03 16:57:15 +0200

    too long. simplify for norminette

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:14 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/03 08:22:37 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/03 16:55:45 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,37 @@
 
 char    *ft_strtrim(char const *s)
 {
-    char *trimd;
-    int i;
-    int j;
-    int k;
+	char *trimd;
+	int i;
+	int j;
+	int k;
 
-    i = 0;
-    j = ft_strlen(s + 1);
-    k = 0;
-    if (!s)
-        return (NULL);
-    if (ft_iswhitespace(s[i]) || ft_iswhitespace(s[j]))
-    {
-        while (ft_iswhitespace(s[i]))
-        {
-            i++;
-            if (s[i] == '\0')
-                return ("");
-        }
-        while (ft_iswhitespace(s[j]))
-            j--;
-    }
-    else
-    {
-        trimd = ft_strnew(j) + 1;
-        return (ft_strcpy(trimd, s));
-    }
-    trimd = ft_strnew((j - i) + 1);
-    if (!trimd)
-        return (0);
-    while (i <= j)
-            trimd[k++] = s[i++];
-    trimd[k] = '\0';
-    return (trimd);
+	i = 0;
+	j = ft_strlen(s + 1);
+	k = 0;
+	if (!s)
+		return (NULL);
+	if (ft_iswhitespace(s[i]) || ft_iswhitespace(s[j]))
+	{
+		while (ft_iswhitespace(s[i]))
+		{
+			i++;
+			if (s[i] == '\0')
+				return ("");
+		}
+		while (ft_iswhitespace(s[j]))
+			j--;
+	}
+	else
+	{
+		trimd = ft_strnew(j) + 1;
+		return (ft_strcpy(trimd, s));
+	}
+	trimd = ft_strnew((j - i) + 1);
+	if (!trimd)
+		return (0);
+	while (i <= j)
+		trimd[k++] = s[i++];
+	trimd[k] = '\0';
+	return (trimd);
 }
