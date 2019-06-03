f623708e2992b68d196568b60a5fbdc8c7fe05e1
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-06-03 08:24:12 +0200

    works in most cases. might not pass norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:14 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/31 14:51:26 by rigardtengelbrecht###   ########.fr       */
+/*   Updated: 2019/06/03 08:22:37 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char    *ft_strtrim(char const *s)
 {
@@ -21,31 +20,31 @@ char    *ft_strtrim(char const *s)
     int k;
 
     i = 0;
-    j = ft_strlen(s);
+    j = ft_strlen(s + 1);
     k = 0;
     if (!s)
         return (NULL);
     if (ft_iswhitespace(s[i]) || ft_iswhitespace(s[j]))
     {
         while (ft_iswhitespace(s[i]))
+        {
             i++;
+            if (s[i] == '\0')
+                return ("");
+        }
         while (ft_iswhitespace(s[j]))
             j--;
     }
     else
     {
-        trimd = ft_strnew(j);
+        trimd = ft_strnew(j) + 1;
         return (ft_strcpy(trimd, s));
     }
-    trimd = ft_strnew((j - i));
+    trimd = ft_strnew((j - i) + 1);
     if (!trimd)
         return (0);
     while (i <= j)
-    {
-            trimd[k] = s[i];
-            i++;
-            k++;
-    }
+            trimd[k++] = s[i++];
     trimd[k] = '\0';
     return (trimd);
 }
