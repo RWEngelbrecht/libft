a755155a4f80a93d4dd753930daf64396e17edca
Rigardt Engelbrecht <rengelbr@c3r6s3.wethinkcode.co.za>
2019-06-03 09:37:14 +0200

    still works

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:40:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/05/27 16:41:00 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/03 07:41:15 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strnew(size_t size)
 {
 	char *str;
 
-	str = (char*)ft_memalloc(sizeof(char) * (size + 1));
+	str = (char*)malloc(sizeof(char) * size + 1);
 	if (!str)
 		return (NULL);
 	ft_bzero(str, size + 1);
