51e152367375621a8182a2b054d013db2e71eb2b
Rigardt Engelbrecht <rengelbr@c3r8s9.wethinkcode.co.za>
2019-06-04 15:41:10 +0200

    fix whitespace skipping function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,38 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:14 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/03 16:55:45 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/04 15:40:42 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strtrim(char const *s)
-{
-	char *trimd;
-	int i;
-	int j;
-	int k;
+-
 
-	i = 0;
-	j = ft_strlen(s + 1);
+char	*ft_strtrim(char const *s)
+{
+	char	*trimd;
+	int		i;
+	int		j;
+	int		k;
+
+	//i = 0;
+	//j = ft_strlen(s + 1);
+	i = skip_whitespace(s, 0);
+	j = skip_whitespace(s, ft_strlen(s + 1));
 	k = 0;
 	if (!s)
 		return (NULL);
+	if (i == 0 || j == 0)
+	{
+		trimd = ft_strnew(ft_strlen(s) + 1);
+		return (ft_strcpy(trimd, s));
+	}
+	/*
 	if (ft_iswhitespace(s[i]) || ft_iswhitespace(s[j]))
 	{
 		while (ft_iswhitespace(s[i]))
-		{
 			i++;
-			if (s[i] == '\0')
-				return ("");
-		}
 		while (ft_iswhitespace(s[j]))
 			j--;
 	}
@@ -40,11 +46,15 @@ char    *ft_strtrim(char const *s)
 		trimd = ft_strnew(j) + 1;
 		return (ft_strcpy(trimd, s));
 	}
-	trimd = ft_strnew((j - i) + 1);
-	if (!trimd)
-		return (0);
-	while (i <= j)
-		trimd[k++] = s[i++];
-	trimd[k] = '\0';
+	*/
+	else
+	{
+		trimd = ft_strnew((j - i) + 1);
+		if (!trimd)
+			return (0);
+		while (i <= j)
+			trimd[k++] = s[i++];
+		trimd[k] = '\0';
+	}
 	return (trimd);
 }
