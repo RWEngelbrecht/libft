982cd36edb05fb7da4a8ebfd58f3b3d5cbc96cce
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-05 10:09:39 +0200

    trying a new approach

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,55 +6,42 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:14 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/04 15:40:42 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/05 10:06:04 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
--
 
 char	*ft_strtrim(char const *s)
 {
 	char	*trimd;
 	int		i;
 	int		j;
-	int		k;
+//	int		k;
 
-	//i = 0;
-	//j = ft_strlen(s + 1);
-	i = skip_whitespace(s, 0);
-	j = skip_whitespace(s, ft_strlen(s + 1));
-	k = 0;
+	i = 0;
+	j = ft_strlen(s + 1);
+//	i = skip_whitespace(s, 0);
+//	j = skip_whitespace(s, ft_strlen(s + 1));
+//	k = 0;
 	if (!s)
 		return (NULL);
-	if (i == 0 || j == 0)
-	{
-		trimd = ft_strnew(ft_strlen(s) + 1);
-		return (ft_strcpy(trimd, s));
-	}
-	/*
-	if (ft_iswhitespace(s[i]) || ft_iswhitespace(s[j]))
-	{
-		while (ft_iswhitespace(s[i]))
-			i++;
-		while (ft_iswhitespace(s[j]))
-			j--;
-	}
-	else
+	while (ft_iswhitespace(s[i]))
+		i++;
+	while (ft_iswhitespace(s[j]))
+		j--;
+/*	if ()
 	{
 		trimd = ft_strnew(j) + 1;
 		return (ft_strcpy(trimd, s));
 	}
-	*/
-	else
-	{
-		trimd = ft_strnew((j - i) + 1);
-		if (!trimd)
-			return (0);
-		while (i <= j)
-			trimd[k++] = s[i++];
-		trimd[k] = '\0';
-	}
+*/
+//	trimd = ft_strnew((j - i) + 1);
+//	if (!trimd)
+//		return (0);
+//	while (i <= j)
+//		trimd[k++] = s[i++];
+	trimd = ft_strsub(s, i, (j - i));	
+//	trimd[k] = '\0';
 	return (trimd);
 }
