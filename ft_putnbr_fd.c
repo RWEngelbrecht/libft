48823517c4e1f69cfd7eb25fc472c51c6d4ae33c
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-06-05 15:25:43 +0200

    used putstr_fd instead - works much better

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 10:54:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/03 13:40:18 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/05 15:23:58 by rigardtengelbrecht###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,18 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		ft_putchar('-');
-		ft_putchar('2');
-		n %= 1000000000;
-		ft_putnbr_fd(-n, fd);
-	}
-	if (n == 2147483648)
-	{
-		ft_putchar('2');
-		n %= 1000000000;
-		ft_putnbr_fd(n, fd);
+		ft_putstr_fd("-2147483648", fd);
 	}
 	else if (n < 0)
 	{
-		ft_putchar('-');
-		ft_putnbr_fd(-n, fd);
+		ft_putchar_fd('-', fd);
+		n = -n;
 	}
-	else if (n > 9)
+	else if (n >= 10)
 	{
 		ft_putnbr_fd((n / 10), fd);
 		ft_putnbr_fd((n % 10), fd);
 	}
 	else
-		ft_putchar(n + '0');
+		ft_putchar_fd(n + '0', fd);
 }
