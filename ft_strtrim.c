778c87d880582f447f006b579958bae0ebd92178
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-06-05 11:38:03 +0200

    works again, and conforms to norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,42 +6,33 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:14 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/05 10:06:04 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/05 11:29:04 by rigardtengelbrecht###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s)
 {
 	char	*trimd;
 	int		i;
 	int		j;
-//	int		k;
 
 	i = 0;
-	j = ft_strlen(s + 1);
-//	i = skip_whitespace(s, 0);
-//	j = skip_whitespace(s, ft_strlen(s + 1));
-//	k = 0;
+	j = ft_strlen(s) - 1;
 	if (!s)
 		return (NULL);
 	while (ft_iswhitespace(s[i]))
-		i++;
-	while (ft_iswhitespace(s[j]))
-		j--;
-/*	if ()
 	{
-		trimd = ft_strnew(j) + 1;
-		return (ft_strcpy(trimd, s));
+		i++;
+		if (s[i] == '\0')
+			return ("");
 	}
-*/
-//	trimd = ft_strnew((j - i) + 1);
-//	if (!trimd)
-//		return (0);
-//	while (i <= j)
-//		trimd[k++] = s[i++];
-	trimd = ft_strsub(s, i, (j - i));	
-//	trimd[k] = '\0';
+	while (ft_iswhitespace(s[j]))
+	{
+		j--;
+	}
+	trimd = ft_strsub(s, i, (j - i) + 1);
 	return (trimd);
 }
