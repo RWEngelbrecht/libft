189574b718ad58c9da71b4c5be72c71e71046659
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-06 14:10:56 +0200

    fixed problem where index variable caused break from comparing loop

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -19,12 +19,13 @@ char	*ft_strnstr(const char *stack, const char *needle, size_t len)
 
 	nd_len = ft_strlen(needle);
 	i = 0;
-	if (nd_len == 0 || needle == stack)
+	printf("%zu\n", nd_len);
+	if (nd_len == 0)
 		return ((char *)stack);
 	while (len > 0 && stack[i] != '\0')
 	{
-		if (!(ft_strncmp((stack + i), needle, nd_len)) && (len > (len - i)))
-			return ((char*)ft_strstr(stack, needle));
+		if (!(ft_strncmp(&(stack[i]), &(needle[i]), nd_len)) && (len >= (len - i)))
+			return ((char*)ft_strstr(&stack[i], &needle[i]));
 		len--;
 		i++;
 	}
