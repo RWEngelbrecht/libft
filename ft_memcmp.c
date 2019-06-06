60e70961ff1ed7a4a4474b5ab42788dc98a4f385
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-06 13:29:33 +0200

    fix for segfault in one specific test

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,21 +14,22 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *str1;
-	unsigned char *str2;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (*str1 == *str2 && n > 1)
+	i = 0;
+	while (i < n && *(str1 + i) == *(str2 + i))
 	{
-		str1++;
-		str2++;
-		n--;
+		i++;
+	//	str1++;
+	//	str2++;
+	//	n--;
 	}
-	if (n == 0)
-	{
+	if (n == i)
 		return (0);
-	}
 	else
-		return (*str1 - *str2);
+		return (*(str1 + i) - *(str2 + i));
 }
