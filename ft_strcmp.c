a79cbdd0ce05a1a7200299f9616badf4bd1ac77a
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-06 17:16:05 +0200

    fixed ft_strcmp return val

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -23,16 +23,19 @@ int		ft_strcmp(const char *s1, const char *s2)
 		s1++;
 		s2++;
 	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	if (((unsigned char)*s1 - (unsigned char)*s2) < 0)
+		return (-1);
+	if (((unsigned char)*s1 - (unsigned char)*s2) > 0)
+		return (1);
+	return (0);
 }
-/*
+
 int main()
 {
-	int a = ft_strcmp("cba", "abc");
-	int b = strcmp("cba", "abc");
+	int a = ft_strcmp("c", "q");
+	int b = strcmp("c", "q");
 
 	printf("ft   |%d|\n", a);
 	printf("libc |%d|\n", b);
 	return 0;
 }
-*/
