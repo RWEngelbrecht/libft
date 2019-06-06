79c99a4dfa4bf624198da7a75358b418afdd006f
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-06 14:03:42 +0200

    tried using indexing

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,14 +14,17 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n > 0 && *s1 != '\0' && *s1 == *s2)
+	size_t i;
+
+	i = 0;
+	while (i < n - 1 && *(s1 + i) != '\0' && *(s1 + i) == *(s2 + i))
 	{
-		s1++;
-		s2++;
-		n--;
+	//	s1++;
+	//	s2++;
+	//	n--;
+		i++;
 	}
-	if (n == 0)
-		return (0);
-	else
-		return ((unsigned char)*s1 - (unsigned char)*s2);
+	if (n)
+		return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
+	return (0);
 }
