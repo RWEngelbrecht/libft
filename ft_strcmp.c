e0ef41ac8206f97666df5bba7b9774deb65754aa
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-06 17:05:12 +0200

    why is libc's strcmp returning what it's returning??????

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -11,13 +11,28 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 != '\0' && *s1 == *s2)
+	int i;
+
+	i = 0;
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
+/*
+int main()
+{
+	int a = ft_strcmp("cba", "abc");
+	int b = strcmp("cba", "abc");
+
+	printf("ft   |%d|\n", a);
+	printf("libc |%d|\n", b);
+	return 0;
+}
+*/
