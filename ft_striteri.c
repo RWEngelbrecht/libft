3985c44ed9953d11c400b48689ce42c0da7dfd75
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-06-06 19:41:28 +0200

    checks for more null params now

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 15:15:53 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/03 13:46:06 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/06 19:39:28 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	unsigned int i;
 
 	i = 0;
-	if (s)
+	if (s && f)
 	{
-		while (s[i] != '\0')
+		while (s[i])
 		{
 			f(i, &s[i]);
 			i++;
