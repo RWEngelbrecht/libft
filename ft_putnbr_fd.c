d96f61d7e71919362f83fe307b04c50472858051
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-06-10 13:33:38 +0200

    replaced else if statements with if statements

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 10:54:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/05 15:32:17 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:09:26 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n == -2147483648)
 	{
 		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		n = -n;
 	}
-	else if (n >= 10)
+	if (n >= 10)
 	{
 		ft_putnbr_fd((n / 10), fd);
-		ft_putnbr_fd((n % 10), fd);
+		ft_putchar_fd((n % 10 + '0'), fd);
 	}
 	else
 		ft_putchar_fd(n + '0', fd);
