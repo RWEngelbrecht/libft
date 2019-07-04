ff83f1d2f2b9226c2eb0d0971451747d836def2a
Rigardt Engelbrecht <rengelbr@c3r8s1.wethinkcode.co.za>
2019-07-04 12:34:13 +0200

    added personal function not specified in WTC's libft project brief

---
+++ ft_strndup.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/04 11:08:31 by rengelbr          #+#    #+#             */
+/*   Updated: 2019/07/04 11:19:52 by rengelbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char *tmp;
+	if (!(tmp = (char*)malloc(sizeof(char) * (ft_strlen(s1) + 1))))
+		return (NULL);
+	return(ft_strncpy(tmp, s1, n));
+}
---
+++ ft_wordcount.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_wordcount.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/04 11:22:35 by rengelbr          #+#    #+#             */
+/*   Updated: 2019/07/04 11:25:12 by rengelbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_wordcount(char const *str, char c)
+{
+	int		count;
+	int		i;
+
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != c && str[i] != '\0')
+			count++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+	}
+	return (count);
+}
