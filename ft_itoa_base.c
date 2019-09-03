28d31cb9aaa4b53deff029df3b5e316e2b827e25
RWEngelbrecht <45070199+RWEngelbrecht@users.noreply.github.com>
2019-09-03 12:42:30 +0200

    converts integer to ascii in 2 < base <= 16

---
+++ ft_itoa_base.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/02 13:00:11 by rengelbr          #+#    #+#             */
+/*   Updated: 2019/09/02 14:15:10 by rengelbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa_base(int value, int base)
+{
+	char	*base_str = "0123456789ABCDEF";
+	char	*ret;
+	long	num;
+	int		len;
+
+	len = 0;
+	if (value == 0)
+		return ("0");
+	if (value == -2147483648 && base == 10)
+		return ("-2147483648");
+	num = value;
+	while (num)
+	{
+		len++;
+		num /= base;
+	}
+	num = value;
+	if (value < 0)
+	{
+		if (base == 10)
+			len++;
+		num *= -1;
+	}
+	ret = (char*)malloc(sizeof(char) * len + 1);
+	ret[len] = '\0';
+	while (num)
+	{
+		ret[--len] = base_str[num % base];
+		num /= base;
+	}
+	if (value < 0 && base == 10)
+		ret[0] = '-';
+	return (ret);
+}
