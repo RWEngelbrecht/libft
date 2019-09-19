39ba6431fd68924503cd1b8b41fa6de8e35ff4de
RWEngelbrecht <45070199+RWEngelbrecht@users.noreply.github.com>
2019-09-19 15:33:29 +0200

    added new funct prototype

--- libft.h
+++ libft.h
@@ -94,5 +94,6 @@ void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 int					ft_int_over(const char *str);
 int					ft_pow(int n, int p);
+int				ft_two_d_arrsize(char **arr);
 
 #endif
